34b38d09f3fbfbed709f38272cfd4d9a69ca259c
arunkumar <arun.udavath@zopsmart.com>
2022-11-28 14:38:49 +0530

    Assignment 1| Arun | updated system info file

--- src/main/java/com/zs/assignment1/SystemInfo.java
+++ src/main/java/com/zs/assignment1/SystemInfo.java
@@ -4,20 +4,12 @@ import java.io.File;
 
 public class SystemInfo {
     public static void main(String[] args){
-        String username= System.getProperty("user.name");
-        String directory= System.getProperty("user.dir");
-        String osBuild= System.getProperty("os.name");
-        String osVersion= System.getProperty("os.version");
-        long diskSpace= (new File("/").getTotalSpace())/-(1024*1024*1024);
-        long memory= Runtime.getRuntime().maxMemory()/-(1024*1024*1024);
-        long processor= Runtime.getRuntime().availableProcessors();
-
-        System.out.println("Name of current User : "+ username);
-        System.out.println("Home directory of current User : "+ directory);
-        System.out.println("System Memory : "+ memory);
-        System.out.println("System CPU/Cores : "+ processor);
-        System.out.println("System disk size : "+ diskSpace);
-        System.out.println("OS build : "+ osBuild);
-        System.out.println("OS Version : "+osVersion);
+        System.out.println("Name of current User : "+ System.getProperty("user.name"));
+        System.out.println("Home directory of current User : "+ System.getProperty("user.dir"););
+        System.out.println("System Memory : "+ Runtime.getRuntime().maxMemory()/-(1024*1024*1024));
+        System.out.println("System CPU/Cores : "+Runtime.getRuntime().availableProcessors());
+        System.out.println("System disk size : "+ (new File("/").getTotalSpace())/-(1024*1024*1024));
+        System.out.println("OS build : "+ System.getProperty("os.name"));
+        System.out.println("OS Version : "+ System.getProperty("os.version"));
     }
 }
